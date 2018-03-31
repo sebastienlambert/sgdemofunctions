65c68bb23264420dd24c35e1fea67be3cc5f2e1e
Sebastien Lambert <sebastienlambert@yahoo.fr>
2018-03-31 12:16:55 +0800

    add get one employee function

---
+++ SGDemoFunctions/Api/HttpGetOneEmployeeFunction_v1.cs
@@ -0,0 +1,55 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using SGDemoFunctions.Domain;
+using MongoDB.Driver;
+using SGDemoFunctions.Infrastructure;
+using System.Security.Authentication;
+using System;
+using System.Threading.Tasks;
+
+namespace SGDemoFunctions.Api
+{
+    public static class HttpGetOneEmployeeFunction_v1
+    {
+        [FunctionName("HttpGetOneEmployeeFunction_v1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/employees/{employeeId}")]HttpRequest req,
+            string employeeId,
+            TraceWriter log)
+        {
+            log.Info($"Get Employee: {employeeId}");
+            var repository = CreateRepository();
+            var employee = await repository.FindOneById(Guid.Parse(employeeId));
+            var dto = new EmployeeDto
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                JobTitle = employee.JobTitle
+            };
+            return new OkObjectResult(dto);
+        }
+
+
+        private static IEmployeeRepository CreateRepository()
+        {
+            string connectionString =
+                @"mongodb://sgdemomongo:GWw0QSGv9Lz2lrPJaVfzJZHzfuZDDImpbAR88BH2y3JVFG1YtwSLFjdp23bwldP9YqOlzkljtJn9lgfAA36IvQ==@sgdemomongo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+              new MongoUrl(connectionString)
+            );
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            var mongoClient = new MongoClient(settings);
+
+            var mongoDatabase = mongoClient.GetDatabase("SgDemo");
+            var employeeCollection = mongoDatabase.GetCollection<Employee>("employee");
+            return new EmployeeRepository(employeeCollection);
+        }
+    }
+}
